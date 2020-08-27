1c6666fb818a0f0521039ff650d149d2640c4ee7
Mohammad Alshoubaki <mohammad.z.alshoubaki@gmail.com>
2020-08-27 10:30:14 -0400

    Add project files.

---
+++ CSharpBasic/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.ComponentModel.DataAnnotations;
+namespace CSharpBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            myString();
+        }
+        static void myString()
+        {
+            Console.WriteLine("Enter you age.");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your name.");
+            string myName = Console.ReadLine();
+
+            Console.WriteLine($"Name: {myName} | Age : {userAge}");
+            string myCoolString = "very cool indeed";
+            string iAmTired = "i am going to use $ with this string.";
+            Console.WriteLine("Cool bro" + myCoolString);
+            Console.WriteLine($"{iAmTired}");
+        }
+    }
+}
