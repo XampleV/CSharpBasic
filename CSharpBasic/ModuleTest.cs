f9e2ea61b46f3a67be48ecab4f57a582214687fa
Mohammad Alshoubaki <mohammad.z.alshoubaki@gmail.com>
2020-09-03 13:29:16 -0400

    new module

---
+++ CSharpBasic/ModuleTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpBasic
+{
+    class ModuleTest
+    {
+        string name;
+        string age;
+        bool hasDog;
+        string dogName;
+
+        // Methods
+
+        void Greeting()
+        {
+            if (hasDog == true)
+            {
+                Console.WriteLine($"Hello {name}. Nice to meet you. I see you have a dog named {dogName} | Age: {age}.");
+            }
+            else
+            {
+                Console.WriteLine($"What {name}!!!!!");
+            }
+        }
+    }
+}
